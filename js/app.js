263c9b23d68aa43e5c7589a1318e6ec4493a8dc0
Thay <pattasiwa@gmail.com>
2021-02-03 05:22:09 -0500

    added win condition, stop board after someone has won

--- js/app.js
+++ js/app.js
@@ -4,34 +4,54 @@ const markBoard = (event) => {
     const $sqDiv = $(event.currentTarget)
     const $xMark = $('<h3>').text('X').addClass('markX')
     const $oMark = $('<h3>').text('O').addClass('markO')
-    if (startValue % 2 === 1) {
-        if ($sqDiv.children().length === 0) {
-            $($sqDiv).append($xMark)
-            console.log($sqDiv.attr('id'))
-            startValue++
+
+    //if gameWin is true then stop appending to the board
+    if (gameWin === false) {
+
+        if (startValue % 2 === 1) {
+            if ($sqDiv.children().length === 0) {
+                $($sqDiv).append($xMark)
+
+                //each time we add a mark we put values to a mapped out board
+                gameBoard[$sqDiv.attr('id')] = 'X'
+                //then we wanna run checkResult
+                console.log(gameBoard)
+                startValue++
+                checkResult()
+
+                console.log(startValue)
+            }
+
+        } else if (startValue % 2 === 0) {
+            if ($sqDiv.children().length === 0) {
+                $($sqDiv).append($oMark)
+                gameBoard[$sqDiv.attr('id')] = 'O'
+                console.log(gameBoard)
+                startValue++
+                checkResult()
+
+                console.log(startValue)
+            }
         }
 
-    } else if (startValue % 2 === 0) {
-        if ($sqDiv.children().length === 0) {
-            $($sqDiv).append($oMark)
-            startValue++
-        }
     }
+
 }
 
 const reset = () => {
+    //reset the whole board and return starting value to 1
+
     $('.square').children('.markX').remove()
     $('.square').children('.markO').remove()
+    startValue = 1
+    const newBoard = [0, 1, 2, 3, 4, 5, 6, 7, 8]
+    gameWin = false
+
+    for (let i = 0; i < 9; i++) {
+        gameBoard[i] = newBoard[i]
+    }
+    $('.gameStatus').text(' ')
 }
-
-
-//////Game Logic////////////
-// can map out the board //
-// give each div an id with numbers///
-//each time we add a mark we put values to a mapped out board
-
-const gameBoardX = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
-const gameBoardO = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
 
 
 
@@ -40,7 +60,7 @@ const gameBoardO = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
 $(() => {
 
     $('.square').on('click', markBoard)
-    $('#reset').on('click', reset)
+    $('.reset').on('click', reset)
 
 
 
---
+++ js/checkWin.js
@@ -0,0 +1,50 @@
+/////Game Logic////////////
+// can map out the board //
+// give each div an id with numbers///
+const gameBoard = [0, 1, 2, 3, 4, 5, 6, 7, 8]
+
+let gameWin = false
+
+const checkResult = () => {
+
+
+
+    //checking all possible wins
+    //check horizontal wins
+    if (gameBoard[0] === gameBoard[1] && gameBoard[0] === gameBoard[2]) {
+        $('.gameStatus').text(`${gameBoard[0]} won the game`)
+        gameWin = true;
+    } else if (gameBoard[3] === gameBoard[4] && gameBoard[3] === gameBoard[5]) {
+        $('.gameStatus').text(`${gameBoard[3]} won the game`)
+        gameWin = true;
+    } else if (gameBoard[6] === gameBoard[7] && gameBoard[6] === gameBoard[8]) {
+        $('.gameStatus').text(`${gameBoard[6]} won the game`)
+        gameWin = true;
+    }//check for vertical wins
+    else if (gameBoard[0] === gameBoard[3] && gameBoard[3] === gameBoard[6]) {
+        $('.gameStatus').text(`${gameBoard[0]} won the game`)
+        gameWin = true;
+    } else if (gameBoard[1] === gameBoard[4] && gameBoard[1] === gameBoard[7]) {
+        $('.gameStatus').text(`${gameBoard[1]} won the game`)
+        gameWin = true;
+    } else if (gameBoard[2] === gameBoard[4] && gameBoard[2] === gameBoard[6]) {
+        $('.gameStatus').text(`${gameBoard[2]} won the game`)
+        gameWin = true;
+    }// check diaganal wins
+    else if (gameBoard[2] === gameBoard[4] && gameBoard[2] === gameBoard[6]) {
+        $('.gameStatus').text(`${gameBoard[2]} won the game`)
+        gameWin = true;
+    } else if (gameBoard[0] === gameBoard[4] && gameBoard[0] === gameBoard[8]) {
+        $('.gameStatus').text(`${gameBoard[0]} won the game`)
+        gameWin = true;
+    }
+
+    // check for draw
+    if (startValue === 10 && gameWin === false) {
+        $('.gameStatus').text(`it's a draw`)
+    }
+}
+
+
+
+
